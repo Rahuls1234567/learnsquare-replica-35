2b2cd143cd6e85cd0b9d6b6abaadc30266a5dcf1
rishu_mehta <rishumehta7370@gmail.com>
2026-08-12 16:32:29 +0000

    Fix Prisma migrations and ignore local artifacts

---
+++ prisma/migrations/20260812000000_add_submission_remarks/migration.sql
@@ -0,0 +1,20 @@
+-- AlterTable
+ALTER TABLE "Enrolment" ADD COLUMN "remarks" TEXT;
+
+-- AlterTable
+ALTER TABLE "TrainingRequest" ADD COLUMN "remarks" TEXT;
+
+-- AlterTable
+ALTER TABLE "Syntaxwork" ADD COLUMN "remarks" TEXT;
+
+-- AlterTable
+ALTER TABLE "Aicas" ADD COLUMN "remarks" TEXT;
+
+-- AlterTable
+ALTER TABLE "TestPrepPro" ADD COLUMN "remarks" TEXT;
+
+-- AlterTable
+ALTER TABLE "Contact" ADD COLUMN "remarks" TEXT;
+
+-- AlterTable
+ALTER TABLE "SemesterPrep" ADD COLUMN "remarks" TEXT;
